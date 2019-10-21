463eeeb23d964af3aabc9601053057459d9996c7
Toshitana <45222739+nanamesan@users.noreply.github.com>
2019-10-21 20:24:27 +0900

    workクラスを作成しました

---
+++ prototype/task.py
@@ -0,0 +1,24 @@
+
+class work():
+    def __init__(self):
+        self.point = None
+        self.real_time = None
+        self.virtual_time = None
+
+    def set_point(self,point):
+        self.point = point
+        return True
+
+    def set_real_time(self, real_time):
+        self.real_time = real_time
+        return True
+
+    def set_virtual_time(self, virtual_time):
+        self.virtual_time = virtual_time
+        return True
+
+    def caliculate_point_per_time(self):
+        return self.real_time/self.point
+
+    def differnt_realtime_from_virtual_time(self):
+        return  abs(self.real_time - self.virtual_time)
