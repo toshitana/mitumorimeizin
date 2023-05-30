646d8a87dab559be41dac830aa499d19e8de0bc0
toshitana <roll2361good@gmail.com>
2023-05-30 16:48:23 +0900

    test typescript

---
+++ src/main.js
@@ -0,0 +1,5 @@
+function hello(word) {
+    return "Hello ".concat(word, "!");
+}
+var message = hello("World");
+console.log(message);
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+function hello(word: string): string {
+    return `Hello ${word}!`;
+  }
+  let message: string = hello("World");
+  console.log(message);
